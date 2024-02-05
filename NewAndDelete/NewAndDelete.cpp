dbeebad32239c668a1bc0e4728e31b0ba199a81b
svechkarev_l <svechkarev_l@DESKTOP-S94NSTH>
2024-02-05 09:08:09 +0500

    Добавьте файлы проекта.

---
+++ NewAndDelete/NewAndDelete.cpp
@@ -0,0 +1,8 @@
+﻿#include <iostream>
+using namespace std;
+
+void sumArr(int* arrA, int* arrB, int* arrC, int size) {
+    for (int i = 0; i < size; i++) {
+        arrC[i] = arrA[i] + arrB[i];
+    }
+}
