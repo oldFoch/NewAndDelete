89c25bce9cf9857d8c4f427d1aa14b7cfc2ff555
svechkarev_l <svechkarev_l@DESKTOP-S94NSTH>
2024-02-05 09:14:21 +0500

    Добавил функции main

--- NewAndDelete/NewAndDelete.cpp
+++ NewAndDelete/NewAndDelete.cpp
@@ -6,3 +6,23 @@ void sumArr(int* arrA, int* arrB, int* arrC, int size) {
         arrC[i] = arrA[i] + arrB[i];
     }
 }
+
+int main() {
+    setlocale(0, "");
+    int size = 5; 
+    int* C;
+    int A[] = { 2, 4, 6, 8, 10 };
+    int B[] = { 1, 3, 5, 7, 9 };
+
+    C = new int[size];
+
+    sumArr(A, B, C, size);
+
+    cout << "Массив C, сумма элементов A и B: ";
+    for (int i = 0; i < size; i++) {
+        cout << C[i] << " ";
+    }
+
+    delete []C;
+    return 0;
+}
